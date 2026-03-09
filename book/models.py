946bbd817134b8c0c832066f638ba7242d0284a6
tidi18 <dastan.temirov19@gmail.com>
2026-03-09 19:06:34 +0600

    модель книг

--- book/models.py
+++ book/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from author.models import Author
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Book(models.Model):
+    authors = models.ManyToManyField(Author)
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField(auto_now_add=True)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        authors_names = ", ".join([str(author) for author in self.authors.all()])
+        return f"{authors_names} {self.title}"
