1d73e03fc0e3bd0c7d7ab53b331ddd49857c4637
tidi18 <dastan.temirov19@gmail.com>
2026-03-09 16:44:01 +0600

    модел автора

--- author/models.py
+++ author/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    biography = models.TextField()
+    date_of_birth = models.DateField()
+    date_of_death  = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
